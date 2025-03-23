782b0fcd1a4c143ee32a13c2d13ec49d8bf3eb6d
Ahmad Pathan <pathanahmad2334@gmail.com>
2025-03-23 13:52:22 +0100

    Improved vite config js to have historyAPIFallback

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  // 👇 Ensures SPA fallback routing (for Render & similar hosts)
+  server: {
+    historyApiFallback: true,
+  }
 })
